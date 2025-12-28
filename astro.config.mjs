a1500e38e40c66a44d6081c2015319266df7ac33
thealbion75 <eddie@thevermeers.co.uk>
2025-12-28 00:44:17 +0000

    @astrojs/cloudflare installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://your-egac-site.pages.dev',
   server: {
     port: 3000
   },
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [react()],
 });
